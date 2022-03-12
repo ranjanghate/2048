3e5117d6af45ee48ec81f3b1fb798b6a516478aa
ranjanghate1 <rghate@gammastack.com>
2022-03-13 00:56:27 +0530

    seting up tiles

---
+++ javascript/app.js
@@ -0,0 +1,9 @@
+import Grid from "./grid.js";
+import Tile from "./tile.js";
+
+
+const gameBoard = document.getElementById('game-board');
+const grid = new Grid(gameBoard);
+
+grid.randomEmptyCell().tile = new Tile(gameBoard);
+grid.randomEmptyCell().tile = new Tile(gameBoard);
--- javascript/script.js
+++
@@ -1,4 +0,0 @@
-import Grid from "./grid.js";
-
-const gameBoard = document.getElementById('game-board');
-const grid = new Grid(gameBoard);
